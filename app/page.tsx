e51cecacafacc022a00859e59a8ba79374fef037
jomelasinasborruel <jomel.borruel@oqulo.com>
2024-03-27 01:00:06 +0800

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,158 @@
-import Image from "next/image";
+"use client";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import Link from "next/link";
+// Components
+import { Model } from "@/models/glasses";
+import { Canvas } from "@react-three/fiber";
+// Utils
+import { delay } from "lodash";
+import clsx from "clsx";
+import zenScroll from "zenscroll";
+// SVGs
+import { GrInstagram } from "react-icons/gr";
+import { ImFacebook, ImLinkedin2 } from "react-icons/im";
+import { BsArrowBarDown } from "react-icons/bs";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+// Styles
+import cx from "./MainPage.module.scss";
 
-export default function Home() {
+const Home = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [videoSectionRef, videoSectionInviewRef] = useInView();
+  const [mainRef, mainInview] = useInView({
+    threshold: 1,
+  });
+  const [hoveredSocial, setHoveredSocial] = useState<number>();
+
+  const handleScroll = () => {
+    zenScroll.toY(1100, 2000);
+    delay(() => {
+      zenScroll.toY(window.scrollY + window.innerHeight, 1000);
+    }, 4000);
+  };
+
+  useEffect(() => {
+    const handleScoll = () => {
+      if (window.scrollY > 1600 || window.scrollY < 0) return;
+      setScrollTop(0.314 * (window.scrollY / 100));
+    };
+
+    document.addEventListener("scroll", handleScoll);
+    setScrollTop(0.314 * (window.scrollY / 100));
+    scrollTo({ top: 0 });
+
+    return () => document.removeEventListener("scroll", handleScoll);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className={cx["main"]}>
+      <div
+        className={clsx(cx["introduction--container"], {
+          "pointer-events-none": mainInview,
+        })}
+      >
+        <Canvas
+          id="glasses-canvas"
+          className={clsx(cx["model-canvas"], {
+            "!opacity-0": videoSectionInviewRef || mainInview,
+          })}
+        >
+          <Suspense fallback={null}>
+            <ambientLight />
+            <Model scrollTop={scrollTop} />
+          </Suspense>
+        </Canvas>
+        <button
+          onClick={handleScroll}
+          className={clsx(cx["btn-scroll"], {
+            "!opacity-0": videoSectionInviewRef,
+          })}
+        >
+          <p>Click to scroll</p>
+
+          <BsArrowBarDown />
+        </button>
+        <div
+          ref={videoSectionRef}
+          className={clsx(cx["video-section"], {
+            "!opacity-100 !pointer-events-auto":
+              videoSectionInviewRef && !mainInview,
+          })}
+        >
+          <video
+            autoPlay
+            playsInline
+            loop
+            muted
+            ref={videoRef}
+            className={clsx(cx["video"])}
+            src="https://res.cloudinary.com/jmcloudname/video/upload/f_auto:video,q_auto/v1/online-portfolio/videos/v9xldgfnoau5kpmzv6uk"
+          />
+          <p className={clsx(cx["video-caption"])}>
+            Rest in reason, <span className="text-white">move in passion.</span>
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div ref={mainRef} className={clsx(cx["proxy-last-section"])}></div>
+      <div
+        className={clsx(cx["main-section"], {
+          "!opacity-100 !pointer-events-auto": mainInview,
+        })}
+      >
+        <div className={cx["details-container"]}>
+          <p className={cx["logo"]}>JM</p>
+          <div className={cx["socials-container"]}>
+            {SOCIALS.map((social, index) => (
+              <Link
+                key={index}
+                href={social.url}
+                target={social.target}
+                onMouseEnter={() => setHoveredSocial(index)}
+                onMouseLeave={() => setHoveredSocial(undefined)}
+                className={clsx({
+                  "!opacity-50":
+                    hoveredSocial !== undefined && hoveredSocial !== index,
+                })}
+              >
+                {social.icons}
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
+
+const SOCIALS: {
+  url: string;
+  icons: JSX.Element;
+  target: React.HTMLAttributeAnchorTarget;
+}[] = [
+  {
+    url: "https://www.facebook.com/UsernameBlocked0/",
+    icons: <ImFacebook />,
+    target: "_blank",
+  },
+  {
+    url: "https://www.instagram.com/jmmmmm_07/",
+    icons: <GrInstagram />,
+    target: "_blank",
+  },
+  {
+    url: "https://www.instagram.com/jmmmmm_07/",
+    icons: <ImLinkedin2 />,
+    target: "_blank",
+  },
+  {
+    url: "mailto:jomelaborruel@gmail.com",
+    icons: <FiMail />,
+    target: "_self",
+  },
+  { url: "tel:09985822142", icons: <FaPhoneAlt />, target: "_self" },
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import defaultTheme from "tailwindcss/defaultTheme";
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    fontFamily: {
+      "earthOrbiter": ["Earth Orbiter", ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [],
